e8560a62d442452c658b14eaf17e46367ed71798
yigitozdamar <yigitozdamar@hotmail.com>
2022-04-07 17:10:54 +0300

    icon color changed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,10 +4,10 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
-  HeartIcon,
   RssIcon,
   LogoutIcon,
 } from '@heroicons/react/outline'
+import { HeartIcon } from '@heroicons/react/solid'
 import { signOut, useSession } from 'next-auth/react'
 import useSpotify from '../hooks/useSpotify'
 import { useRecoilState } from 'recoil'
@@ -56,11 +56,11 @@ function Sidebar() {
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5 text-red-300" />
           <p>Liked Songs</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+        <button className="flex items-center space-x-2  hover:text-white">
+          <RssIcon className="h-5 w-5 text-green-400" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
